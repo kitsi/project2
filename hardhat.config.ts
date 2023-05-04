352dc9e35089ae493757c01cb4de9fab8a203216
KS <kitsidhu@gmail.com>
2023-05-04 11:52:02 -0400

    save bef clearing template files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,15 @@
 import "@nomicfoundation/hardhat-toolbox";
 
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//     console.log(await account.getBalance());
+//   }
+// });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
